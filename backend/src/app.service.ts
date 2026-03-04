023a0a1c3386c94e64376c6ac3ea57bc87432609
Trisha <agarwaltrisha47@gmail.com>
2026-03-04 18:29:56 +0530

    Update app service

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async testDb() {
+    const count = await this.prisma.lead.count();
+    return { leads: count };
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
 }
+
+
+
