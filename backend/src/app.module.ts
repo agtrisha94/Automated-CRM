0cf4e236dcbe5206355c1b9aa491cc70c591ee90
Trisha <agarwaltrisha47@gmail.com>
2026-03-19 21:56:55 +0530

    chore: update app module configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { LeadsModule } from './leads/leads.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }), // ← add this
+    ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
+    LeadsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
